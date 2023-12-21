f6da2e591ae2e6817424d3b34b0d84951711227b
neisnei <neischalyruidiaz@gmail.com>
2023-12-21 18:38:49 +0000

    cities

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-""" objects handles RestFul API actions for cities """
-
+""" objects that handles all default RestFul API actions for cities """
 from models.city import City
 from models.state import State
 from models import storage
@@ -9,59 +8,58 @@ from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 
 
-@app_views.route("/states/<state_id>/cities", methods=["GET"])
-def get_state_cities(state_id):
-    state = State.get(state_id)
+@app_views.route('/states/<state_id>/cities', methods=['GET'],
+                 strict_slashes=False)
+@swag_from('documentation/city/cities_by_state.yml', methods=['GET'])
+@cities_bp.route('/states/<state_id>/cities', methods=['GET'])
+def get_cities_by_state(state_id):
+    state = State.query.get(state_id)
     if not state:
         abort(404)
-    cities = [city.to_dict() for city in state.cities]
-    return jsonify(cities)
+    cities = City.query.filter_by(state_id=state_id).all()
+    return jsonify([city.to_dict() for city in cities])
 
-
-@app_views.route("/cities/<city_id>", methods=["GET"])
+@cities_bp.route('/cities/<city_id>', methods=['GET'])
 def get_city(city_id):
-    city = City.get(city_id)
+    city = City.query.get(city_id)
     if not city:
         abort(404)
     return jsonify(city.to_dict())
 
-
-@app_views.route("/cities/<city_id>", methods=["DELETE"])
+@cities_bp.route('/cities/<city_id>', methods=['DELETE'])
 def delete_city(city_id):
-    city = City.get(city_id)
+    city = City.query.get(city_id)
     if not city:
         abort(404)
-    city.delete()
+    db.session.delete(city)
+    db.session.commit()
     return jsonify({}), 200
 
-
-@app_views.route("/states/<state_id>/cities", methods=["POST"])
+@cities_bp.route('/states/<state_id>/cities', methods=['POST'])
 def create_city(state_id):
-    state = State.get(state_id)
+    state = State.query.get(state_id)
     if not state:
         abort(404)
-    if not request.is_json:
-        abort(400, "Not a JSON")
     data = request.get_json()
-    if "name" not in data:
-        abort(400, "Missing name")
-    city = City()
-    city.state_id = state_id
-    city.name = data["name"]
-    city.save()
+    if not data:
+        abort(400, 'Not a JSON')
+    if 'name' not in data:
+        abort(400, 'Missing name')
+    city = City(name=data['name'], state_id=state_id)
+    db.session.add(city)
+    db.session.commit()
     return jsonify(city.to_dict()), 201
 
-
-@app_views.route("/cities/<city_id>", methods=["PUT"])
+@cities_bp.route('/cities/<city_id>', methods=['PUT'])
 def update_city(city_id):
-    city = City.get(city_id)
+    city = City.query.get(city_id)
     if not city:
         abort(404)
-    if not request.is_json:
-        abort(400, "Not a JSON")
     data = request.get_json()
+    if not data:
+        abort(400, 'Not a JSON')
     for key, value in data.items():
-        if key not in ["id", "state_id", "created_at", "updated_at"]:
+        if key not in ['id', 'state_id', 'created_at', 'updated_at']:
             setattr(city, key, value)
-    city.save()
+    db.session.commit()
     return jsonify(city.to_dict()), 200
