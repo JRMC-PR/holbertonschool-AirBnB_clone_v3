e48819fb913f1208480cbc8a210f84000bdff2de
neisnei <neischalyruidiaz@gmail.com>
2023-12-21 18:26:09 +0000

    cities

---
+++ api/v1/views/cities.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+""" objects handles RestFul API actions for cities """
+
+from flask import abort, jsonify, request
+from api.v1.views import app_views
+from models.city import City
+from models.state import State
+
+
+@app_views.route("/states/<state_id>/cities", methods=["GET"])
+def get_state_cities(state_id):
+    state = State.get(state_id)
+    if not state:
+        abort(404)
+    cities = [city.to_dict() for city in state.cities]
+    return jsonify(cities)
+
+
+@app_views.route("/cities/<city_id>", methods=["GET"])
+def get_city(city_id):
+    city = City.get(city_id)
+    if not city:
+        abort(404)
+    return jsonify(city.to_dict())
+
+
+@app_views.route("/cities/<city_id>", methods=["DELETE"])
+def delete_city(city_id):
+    city = City.get(city_id)
+    if not city:
+        abort(404)
+    city.delete()
+    return jsonify({}), 200
+
+
+@app_views.route("/states/<state_id>/cities", methods=["POST"])
+def create_city(state_id):
+    state = State.get(state_id)
+    if not state:
+        abort(404)
+    if not request.is_json:
+        abort(400, "Not a JSON")
+    data = request.get_json()
+    if "name" not in data:
+        abort(400, "Missing name")
+    city = City()
+    city.state_id = state_id
+    city.name = data["name"]
+    city.save()
+    return jsonify(city.to_dict()), 201
+
+
+@app_views.route("/cities/<city_id>", methods=["PUT"])
+def update_city(city_id):
+    city = City.get(city_id)
+    if not city:
+        abort(404)
+    if not request.is_json:
+        abort(400, "Not a JSON")
+    data = request.get_json()
+    for key, value in data.items():
+        if key not in ["id", "state_id", "created_at", "updated_at"]:
+            setattr(city, key, value)
+    city.save()
+    return jsonify(city.to_dict()), 200
