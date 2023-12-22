e50f0c8c62af74195774170e09bc30532b49a7be
JRMC-PR <jesus.rafael.mendez.cruz@gmail.com>
2023-12-22 09:00:14 -0400

    Update bd and file storage

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -80,27 +80,16 @@ class DBStorage:
         Returns the object based on the class name and its ID, or
         None if not found
         """
-        if cls not in classes.values():
-            return None
 
-        all_cls = models.storage.all(cls)
-        for value in all_cls.values():
+        for value in self.all(cls).values():
             if (value.id == id):
                 return value
-
         return None
 
     def count(self, cls=None):
         """
         count the number of objects in storage
         """
-        all_class = classes.values()
-
-        if not cls:
-            count = 0
-            for clas in all_class:
-                count += len(models.storage.all(clas).values())
-        else:
-            count = len(models.storage.all(cls).values())
-
-        return count
+        if cls:
+            return len(self.all(cls))
+        return len(self.all())
